ad8d670bc03b6d5d93485e3b47867407e7cc8f4b
mhmd <ms.mhmdsalah@gmail.com>
2024-02-11 18:54:07 +0200

    the end

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-// hello dear;
+let scrollContainer = document.querySelector( ".gallery" );
+let backbtn = document.getElementById( "backBtn" );
+let nextBtn = document.getElementById( "nextBtn" );
+
+scrollContainer.addEventListener( "wheel", ( event ) => {
+  event.preventDefault();
+  scrollContainer.scrollLeft += event.deltaY;
+  scrollContainer.style.scrollBehavior = "auto"
+} );
+
+nextBtn.addEventListener( "click", () => {
+  scrollContainer.style.scrollBehavior = "smooth"
+  scrollContainer.scrollLeft += 900;
+})
+scrollContainer.style.scrollBehavior = "smooth"
+backbtn.addEventListener( "click", () => {
+  scrollContainer.scrollLeft -= 900;
+})
